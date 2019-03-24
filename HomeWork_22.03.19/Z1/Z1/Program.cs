0243b32bd46f0a94ba1f7cd4b5148b060aa5c668
RuslanNv <vegapunk1707@gmail.com>
2019-03-25 00:08:15 +0300

    hw4

---
+++ HomeWork_22.03.19/Z1/Z1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Z1
+{
+	class Program
+	{
+		public static void Main()
+		{
+			string str = "Ехал Грека Через Реку Видит Грека В Реке Рак сунул грека руку в реку рак за руку греку ЦАП";
+			string lowerString = str.ToLower();
+			string[] array = lowerString.Split(' ');
+			Array.Sort(array);
+			List<WordInfo> listOfWords = new List<WordInfo>();
+			for(int i = 0, l = 0; i < array.Length; l++)
+			{
+                int step = 1;
+                listOfWords.Add(new WordInfo(array[i]));
+				for(int j = i + 1; j < array.Length ;j++)
+				{
+					if(array[j] == array[i])
+					{
+						listOfWords[l].Count++;
+						step++;
+				    }
+                }
+                i += step;
+			}
+			Console.WriteLine();
+			foreach(WordInfo wf in listOfWords)
+				Console.WriteLine("{0}: {1}", wf.Word, wf.Count);
+			Console.ReadLine();
+		}
+	}
+}
---
+++ HomeWork_22.03.19/Z1/Z1/WordInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Z1
+{
+	public class WordInfo
+	{
+		public string Word { get; set; }
+		public int Count { get; set; }
+
+        public WordInfo(string word) { Word = word; Count = 1;}
+	}
+}
